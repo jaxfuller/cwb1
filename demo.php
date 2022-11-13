be4fc2921bf71031855c2b8a10a6d7c852b98906
jaxfuller <jaxful@gmail.com>
2022-11-13 12:16:47 -0700

    added demo php code from class

--- demo.php
+++ demo.php
@@ -1,7 +1,23 @@
-<?php
-$subject = ";
+<?php print "<!doctype html>
+<html lang=\"en\">
+<head>
+<title>Form Confirmation Page</title>
+</head>
+<body>
+<hl >The form information received is given below:</h l >";
+$message = ";
 foreach ($_pOST as $key=> $value){
-$subject.= $key . " : " . $value . "<br>\r\n";
+$message.= $key . " : " . $value . "<br>\r\n";
 
-print $message; print "
+print $message; print "<hr>
+<hr>
+<hr>
+<br>I
+< hr >
+< hr >
+< form action=\"#\">
+<input type=\"button\" value  = \"Back\" onclick=\"javascript:history.go(-1)\" />
+</form>
+</body>
+</html>"
 ?>
